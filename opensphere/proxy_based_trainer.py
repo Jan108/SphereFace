8e3d78fd0da914ec2a43c17d37f384bebaf8b091
Jan108 <jan@thebraker.net>
2026-03-15 14:36:28 +0100

    less logging

--- opensphere/proxy_based_trainer.py
+++ opensphere/proxy_based_trainer.py
@@ -25,7 +25,8 @@ class ProxyBasedTrainer(BaseTrainer):
         data, labels = data.to(self.rank), labels.to(self.rank)
 
         def log_stat(t, name):
-            self.train_logger.logger.info(f'{name} min: {torch.min(t)}, max: {torch.max(t)}, any inf: {torch.isinf(t).any()}, any nan: {torch.isnan(t).any()}')
+            if self.step % 25 == 0:
+                self.train_logger.logger.info(f'{name} min: {torch.min(t)}, max: {torch.max(t)}, any inf: {torch.isinf(t).any()}, any nan: {torch.isnan(t).any()}')
 
         log_stat(data, 'data')
         log_stat(labels, 'labels')
@@ -40,11 +41,11 @@ class ProxyBasedTrainer(BaseTrainer):
             feats = self.model.backbone(data)
             log_stat(feats, 'backbone')
 
-            self.train_logger.logger.info('='*100)
-            sleep(10)
-
             loss = self.model.head(feats, labels)
             log_stat(loss, 'head')
+
+        if self.step % 25 == 0:
+            self.train_logger.logger.info('='*100)
 
         # backward
         self.scaler.scale(loss).backward()
