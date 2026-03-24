d86baa9a011a12a92e3108d87fc19d799686400d
Jan108 <jan@thebraker.net>
2026-03-24 16:05:26 +0100

    finished eval face rec

--- evaluation.py
+++ evaluation.py
@@ -83,10 +83,20 @@ def verification(params):
         F.normalize(net(img2.to(device)))
         end_time = datetime.now()
         inf_times.append(end_time - start_time)
-    avg_time = sum(inf_times, timedelta()) / len(inf_times)
-    print(f'Inference for {params.proj_dir} took {avg_time}')
-    with open(os.path.join(params.proj_dir, 'timing.txt'), 'w') as file:
-        file.write(str(avg_time))
+    avg_time_pair = sum(inf_times, start=timedelta()) / len(inf_times)
+
+    inf_times = []
+    for img1, img2, label in tqdm(islice(test_loader_latency, params.latency_test),
+                                  desc='Test image pairs latency', total=params.latency_test):
+        start_time = datetime.now()
+        F.normalize(net(img1.to(device)))
+        end_time = datetime.now()
+        inf_times.append(end_time - start_time)
+    avg_time_single = sum(inf_times, start=timedelta()) / len(inf_times)
+    print(f'Inference for {params.weights} took pair: {avg_time_pair}, single: {avg_time_single}')
+    with open(os.path.join(params.output, 'timing.txt'), 'w') as file:
+        file.write(str(avg_time_pair)+'\n')
+        file.write(str(avg_time_single)+'\n')
 
 
 def identification(params):
@@ -123,29 +133,36 @@ def identification(params):
     pool_features, pool_labels = torch.cat(features, dim=0), labels
     pool_features = pool_features.to(device)
 
+    all_labels = []
+    all_sims = []
+    all_indices = []
+
+    # Process test images in batches
+    for test_images, test_labels in tqdm(test_loader, desc="Processing test images"):
+        test_images = test_images.to(device)
+        test_features = net(test_images)
+        test_features = F.normalize(test_features, p=2, dim=1)
+
+        # Compute cosine similarity (dot product of normalized vectors)
+        sim = torch.matmul(test_features, pool_features.T)  # (batch_size, num_pool)
+        # Get top 5 similarities and indices
+        top5_sim, top5_indices = torch.topk(sim, k=5, dim=1)
+
+        pred_labels = [
+            [pool_labels[i] for i in topk] for topk in top5_indices.tolist()
+        ]
+
+        all_labels.extend(test_labels.tolist())
+        all_sims.extend(top5_sim.tolist())
+        all_indices.extend(pred_labels)
+
     # Open output file
-    with open(os.path.join(params.proj_dir, 'identification.csv'), 'w') as f:
-        f.write("test_label,predicted_label\n")
-
-        # Process test images in batches
-        for test_images, test_labels in tqdm(test_loader, desc="Processing test images"):
-            test_images = test_images.to(device)
-            test_features = net(test_images)
-            test_features = F.normalize(test_features, p=2, dim=1)
-
-            # Compute cosine similarity (dot product of normalized vectors)
-            sim = torch.matmul(test_features, pool_features.T)  # (batch_size, num_pool)
-            max_sim, indices = torch.max(sim, dim=1)
-
-            # Determine predicted labels
-            predicted_labels = [
-                pool_labels[i] if sim > 0 else -1
-                for sim, i in zip(max_sim.tolist(), indices.tolist())
-            ]
-
-            # Write results
-            for test_label, pred_label in zip(test_labels.tolist(), predicted_labels):
-                f.write(f"{test_label},{pred_label}\n")
+    os.makedirs(os.path.dirname(params.output), exist_ok=True)
+    with open(os.path.join(params.output, params.identification_file), 'w') as f:
+        f.write(
+            f"test_label,{','.join([f'predicted_label_{i}' for i in range(5)])},{','.join([f'similarity_{i}' for i in range(5)])}\n")
+        for test_label, pred_label, sim in zip(all_labels, all_indices, all_sims):
+            f.write(f"{test_label},{','.join([str(i) for i in pred_label])},{','.join([str(i) for i in sim])}\n")
 
 
 if __name__ == "__main__":
@@ -157,6 +174,16 @@ if __name__ == "__main__":
     parser.add_argument("--img_identification", type=str, default='./data/split/identification.csv',
                         help="File containing a list of images files with corresponding label for identification")
     parser.add_argument("--latency_test", type=int, default=1000, help="Amount of images for the latency test")
+    parser.add_argument("--ident-general", action='store_true', help="Generalized model evaluation")
     args = parser.parse_args()
     verification(args)
-    identification(args)
+    args.identification_file = 'identification.csv'
+    if args.ident_general:
+        base_path_ident = args.img_identification
+        print('Loading and performing each class separately for generalized model')
+        for cls in ['bird', 'cat', 'dog', 'small_animals']:
+            args.img_identification = f'{base_path_ident}/{cls}/identification_img.csv'
+            args.identification_file = f'identification_{cls}.csv'
+            identification(args)
+    else:
+        identification(args)
